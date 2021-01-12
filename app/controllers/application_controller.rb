1d395fc731556ff143a57f942a04c08396f2a7a1
Nicolás Nisoria <nicolas.nisoria@globallogic.com>
2021-01-12 18:39:24 -0300

    Add api scope and root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  def welcome
+    render json: "Welcome to #{Rails.application.class.module_parent.name}"
+  end
 end
